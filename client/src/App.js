90571560a9f57ee36804d873b96f4c9eef2c0a23
Dhanishkaa-1801 <dhanishkaabiz@gmail.com>
2026-07-12 17:19:15 +0530

    feat: updated graph structure

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import Login from './pages/Login';
 import Chat from './pages/Chat';
 import Analytics from './pages/Analytics';
 import Search from './pages/Search';
+import Graph from './pages/Graph';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"          element={<Landing />} />
-        <Route path="/login"     element={<Login />} />
-        <Route path="/chat"      element={<Chat />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="/analytics" element={<Analytics />} />
-        <Route path="/search"    element={<Search />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/graph" element={<Graph />} />
       </Routes>
     </BrowserRouter>
   );
